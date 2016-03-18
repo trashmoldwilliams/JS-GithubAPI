79ffbf20a5c12a18ca75ebdf7d29d9cfab398ca0
Will Johnson <trashmoldwilliams@gmail.com>
2016-03-18 09:40:59 -0700

    Page outputs information on user's repos.

--- js/getRepos.js
+++ js/getRepos.js
@@ -16,4 +16,20 @@ exports.getRepos = function(username){
     console.log(error.responseJSON.message);
   });
 
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    for(var i = 0; i < response.length; i++) {
+      $("#projectList").append("<li>" + response[i].name + "<ul>" +
+                                "<li>Description: " + response[i].description + "</li>" +
+                                "<li>Language: " + response[i].language + "</li>" +
+                                "<li>Created at: " + response[i].created_at.substring(0,10) + "</li>" +
+                                "<li>url: " + response[i].url + "</li>" +
+                                "</ul></li>")
+    }
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+
 };
