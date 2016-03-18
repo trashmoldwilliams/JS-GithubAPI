aebc5a93dcfe01dc175217697a4eac1db24a45d0
Will Johnson <trashmoldwilliams@gmail.com>
2016-03-18 09:21:01 -0700

    Page outputs basic info about selected gh user.

--- js/getRepos.js
+++ js/getRepos.js
@@ -4,6 +4,13 @@ exports.getRepos = function(username){
 
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('#avatarContainer').append("<img src='" + response.avatar_url + "'>");
+    $('#usernameOutput').append(response.login);
+    $('#nameOutput').append(response.name);
+    $('#followersCount').append(response.followers);
+    $('#followingCount').append(response.following);
+    $('#repoCount').append(response.public_repos);
+
 
   }).fail(function(error){
     console.log(error.responseJSON.message);
